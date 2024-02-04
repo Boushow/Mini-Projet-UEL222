78e82afe1f8c571aa1be992ac55e90a7d635fe30
mohamed <aldanmarki77@gmail.com>
2024-02-04 20:05:17 +0100

    test

---
+++ migrations/Version20240204175856.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240204175856 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE notation (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, valeur INTEGER DEFAULT NULL, commentaire VARCHAR(255) DEFAULT NULL, return VARCHAR(255) DEFAULT NULL)');
+        $this->addSql('CREATE TABLE value (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE notation');
+        $this->addSql('DROP TABLE value');
+    }
+}
